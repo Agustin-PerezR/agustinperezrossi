2a397843b0e4dacce88f7bb1cfea61f7de984b53
Agustin-PerezR <72412854+Agustin-PerezR@users.noreply.github.com>
2021-07-14 11:36:11 -0300

    Update Sistema.java

--- Laboratorio/com/company/IntegradorTarjeta/Sistema.java
+++ Laboratorio/com/company/IntegradorTarjeta/Sistema.java
@@ -4,15 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
-/*La clase Sistema tendrá como métodos:
 
-    "subePasajero(Pasajero pasajero)", método que no retorna nada; imprime "¡Bienvenido!" en caso de que el usuario haya podido pagar su boleto exitosamente e imprime "Saldo Insuficiente" en caso contrario.
-    "seBajaPasajero(Pasajero pasajero)", método que no retorna nada
-    "pasajerosQueSeHanSubidoAlgunaVez()", retorna una instancia de clase HashSet
-    "pasajerosConUltimoMontoAbonado()", retorna una instancia de la clase HashMap
-    "ultimoMontoAbonadoPorPasajero(float numeroId)", retorna el monto del pasajero que tiene la tarjeta con numeroId Utilizar el método "pasajerosConUltimoMontoAbonado()"
-    "seSuperaElSaldoNegativo()", método que retorna true en caso de superar el saldo negativo y false en caso contrario
-*/
 public class Sistema {
     private ArrayList<Pasajero> pasajerosPresentes;
     private ArrayList<Pasajero> historialDePasajeros;
