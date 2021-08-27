d0bf0ee695cb09597a83a050ed96b7c0013bccd0
Agustin-PerezR <72412854+Agustin-PerezR@users.noreply.github.com>
2021-08-27 14:09:33 -0300

    Add files via upload

--- JS Perez Rossi/pagina.js
+++ JS Perez Rossi/pagina.js
@@ -1,11 +1,11 @@
 let e;
-window.addEventListener("scroll", function(e) {
-    e= window.scrollY;
-    console.log(e);
-    if(e=> 100 ){
+/*window.addEventListener("scroll", ()=> {
+    const total= window.scrollY;
+    console.log(total);
+    if(total => 100 ){
         document.getElementById("trabajos").style.backgroundImage="linear-gradient(90deg, rgb(0, 36, 212), rgb(218, 135, 125))"
     }
-    else if(e => 600){
+    else if(total => 600){
         document.getElementById("trabajo").style.backgroundImage="linear-gradient(45deg, rgb(136, 136, 245), rgb(248, 135, 135))"
     }
-});
+});*/
