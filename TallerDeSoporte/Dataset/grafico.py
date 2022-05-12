f0ec7111564ce05e72cb4c2472995860b3f81fc9
Agustin-PerezR <72412854+Agustin-PerezR@users.noreply.github.com>
2022-05-12 08:53:20 -0300

    Add files via upload

---
+++ TallerDeSoporte/Dataset/grafico.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt1
+import matplotlib.pyplot as plt2
+
+df1 = pd.read_csv("/content/archivo.csv")
+df1.set_index('mes')
+df1.loc[0:3]
+x_values = df1['mes'].unique()
+y_values = df1['mes'].value_counts().tolist()
+y_values.reverse()
+print(x_values)
+print(y_values)
+# gráfico 1
+plt1.bar(x_values,y_values)
+plt1.title('delitos')
+plt1.xlabel('mes')
+plt1.ylabel('tipo')
+plt1.show()
